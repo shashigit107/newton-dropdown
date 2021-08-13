7db287d4e3665599758b8846a730bb512a4a2eb4
shashigit107 <shashiranjan107@gmail.com>
2021-08-14 02:12:31 +0530

    initail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import states from './data'
+import react, { useState } from "react"
 
 function App() {
+  const [stateVal, setState] = useState(0);
+  const [cityVal, setCity] = useState(0);
+  const [landVal, setLand] = useState(0);
+  const HandleState = (e) => {
+    setState(e.target.value);
+  }
+  const HandleCity = (e) => {
+    setCity(e.target.value)
+  }
+  const HandleLand = (e) => {
+    setLand(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select id="state" value={stateVal} onChange={HandleState}>
+        {states.map((para, index) => {
+          return <option value={index}>{para.name}</option>
+        })}
+        {console.log(stateVal)}
+      </select>
+
+
+      <select id="city" value={cityVal} onChange={HandleCity}>
+        {states[stateVal].city.map((para, index) => {
+          return <option value={index}>{para.name}</option>
+        })}
+      </select>
+
+
+      <select id="landmark" value={landVal} onChange={HandleLand}>
+        {states[stateVal].city[cityVal].landmarks.map((para, index) => {
+          return <option value={index}>{para.name}</option>
+        })}
+      </select>
+
+      <div id="state-title">{states[stateVal].name}</div>
+      <div id="state-description">{states[stateVal].description}</div>
+       
+      <div id="city-title">{states[stateVal].city[cityVal].name}</div>
+      <div id="city-description">{states[stateVal].city[cityVal].description}</div>
+
+      <div id="landmark-title">{states[stateVal].city[cityVal].landmarks[landVal].name}</div>
+      <div id="landmark-description">{states[stateVal].city[cityVal].landmarks[landVal].description}</div>
+
+
     </div>
   );
 }
